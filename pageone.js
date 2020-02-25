27c9714e30d1686eab8a9025cd854e8af3860500
rannumianda280792 <rannumianda92@gmail.com>
2020-02-25 21:50:38 +0800

    buat pageone.js isian popup dan while

---
+++ pageone.js
@@ -0,0 +1,10 @@
+alert("ini page pertama javascript");
+var nama = prompt("Selamat datang, tuliskan nama anda : "),
+    alamat = prompt("masukan alamat anda : ");
+while (nama == '' || alamat == '') {
+    nama = prompt('mohon input nama anda : ');
+    alamat = prompt('mohon input alamat lengkap anda : ');
+}
+alert = ('silahkan cek nama dan alamat anda di console');
+console.log('nama : ' + nama);
+console.log('alamat : ' + alamat);
